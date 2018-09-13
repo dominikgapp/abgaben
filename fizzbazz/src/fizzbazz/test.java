b38f983218bb7c1f2fc822c3bcac9faeab4471aa
Dominik  Gapp <dominikgapp@Dominiks-MacBook-Air.local>
2018-09-13 16:12:52 +0200

    Beispiel fizzbazz

---
+++ fizzbazz/src/fizzbazz/test.java
@@ -0,0 +1,39 @@
+package fizzbazz; 
+
+public class test {
+	
+	
+	   public static void main (String[] args) {
+	
+	for (int i=1; i<=100; i++)
+	{
+		if (i%3 != 0 && i%4 != 0)
+		{
+			System.out.println(i);
+		}
+	
+	
+		if (i%3 == 0 && i%4 == 0)
+		{
+			System.out.println("fizzbazz");
+		} else if
+		
+	(i%3 == 0)
+	{
+		System.out.println("fizz");
+	}else if
+	
+	(i %4 == 0)
+	{
+		System.out.println("bazz");
+	}
+	
+	
+	 
+	}
+	
+	   }
+	
+}
+
+
